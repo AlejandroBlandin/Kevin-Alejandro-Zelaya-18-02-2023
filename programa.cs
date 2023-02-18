2fe905b47dd6391223f7e24afad9e1e763d1b45d
AlejandroBlandin <zb0601042019@unab.edu.sv>
2023-02-18 11:56:03 -0600

    cambios

--- programa.cs
+++ programa.cs
@@ -1,1 +1,4 @@
-Console.WriteLine("HOla");
+Console.WriteLine("Escriba una palabra");
+string palabra = Console.ReadLine();
+int cantidadLetras = palabra.legnth;
+Console.WriteLine("La cantidad de letras en que hay en la palabra que escribio es de {0}", palabra);
